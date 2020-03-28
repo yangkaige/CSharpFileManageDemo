6a906d3fa879732b04378781efb59cc411f77497
yangkaige <1546865912@qq.com>
2020-03-28 11:08:14 +0800

    添加文件管理器小示例

--- FileManageApp/Form1.Designer.cs
+++ FileManageApp/Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnOpenFolder);
+            this.splitContainer1.Panel2.Controls.Add(this.txtContent);
+            this.splitContainer1.Size = new System.Drawing.Size(802, 515);
+            this.splitContainer1.SplitterDistance = 267;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView
+            // 
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView.Location = new System.Drawing.Point(0, 0);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(267, 515);
+            this.treeView.TabIndex = 0;
+            this.treeView.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView_NodeMouseDoubleClick);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(3, 41);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(525, 474);
+            this.txtContent.TabIndex = 0;
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(3, 12);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFolder.TabIndex = 1;
+            this.btnOpenFolder.Text = "选择文件夹";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(802, 515);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.TextBox txtContent;
     }
 }
 
--- FileManageApp/Form1.cs
+++ FileManageApp/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace FileManageApp
 {
     public partial class Form1 : Form
@@ -15,6 +15,62 @@ namespace FileManageApp
         public Form1()
         {
             InitializeComponent();
+        }
+        private string folder;
+        /// <summary>
+        /// 打开文件夹
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            folder = fbd.SelectedPath;
+            if (string.IsNullOrEmpty(folder))
+            {
+                return;
+            }
+            treeView.Nodes.Clear();
+            txtContent.Text = "";
+            LoadData(folder, treeView.Nodes);
+        }
+        /// <summary>
+        /// 获取目录信息
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <param name="nodes"></param>
+        private void LoadData(string folder, TreeNodeCollection nodes)
+        {
+            string[] dics = Directory.GetDirectories(folder);
+            string[] filePaths = Directory.GetFiles(folder);
+            //加载目录
+            foreach (var item in dics)
+            {
+                TreeNode treeNode = nodes.Add(Path.GetFileName(item));
+                LoadData(item, treeNode.Nodes);
+            }
+            //加载文件
+            foreach (var item in filePaths)
+            {
+                TreeNode treeNode = nodes.Add(Path.GetFileName(item));
+                treeNode.Tag = item;
+            }
+        }
+        /// <summary>
+        /// 双击树控件节点
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void treeView_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode treeNode = treeView.SelectedNode;
+            string path = treeNode.Tag.ToString();
+            if (string.IsNullOrEmpty(path))
+            {
+                return;
+            }
+            txtContent.Text = File.ReadAllText(path,Encoding.Default);
         }
     }
 }
